0af6928622160911510aaaa2bae6a2d03b948207
Howard <Howard@aitc.com.tw>
2022-09-01 00:26:35 +0800

    add first gantt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { NgxGanttModule } from '@worktile/gantt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { EmployeeComponent } from './employee/employee.component';
     BrowserModule,
     MatSliderModule, 
     MatButtonModule,
+    NgxGanttModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GanttItem } from '@worktile/gantt';
 
 @Component({
   selector: 'app-schedule',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-
+  items: GanttItem[] = [
+    { id: '000000', title: 'Task 0', start: 1627729997, end: 1628421197, expandable: true },
+    { id: '000001', title: 'Task 1', start: 1617361997, end: 1625483597, links: ['000003', '000004', '000000'], expandable: true },
+    { id: '000002', title: 'Task 2', start: 1610536397, end: 1610622797 },
+    { id: '000003', title: 'Task 3', start: 1628507597, end: 1633345997, expandable: true }
+  ];
   constructor() { }
 
   ngOnInit(): void {
