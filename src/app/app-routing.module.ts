499ceeec922e21715bbe39a894a70dcd93bfb8ab
Howard <Howard@aitc.com.tw>
2022-08-31 23:39:41 +0800

    add Angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  },
+  {
+    path: 'employee',
+    component: EmployeeComponent
+  },
+  {
+    path: '',
+    component: ScheduleComponent
+  },
+  {
+    path: '**',
+    redirectTo:'schedule',
+    pathMatch:'full'
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatSliderModule, 
+    MatButtonModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
