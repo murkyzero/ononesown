2789bb8ba04c8371e3006b7e3023ef14cca99245
Howard <Howard@aitc.com.tw>
2022-09-01 23:32:47 +0800

    mkdir frontend and move

--- frontend/src/app/schedule/schedule.component.ts
+++ frontend/src/app/schedule/schedule.component.ts
@@ -1,3 +1,4 @@
+import { DIR_DOCUMENT } from '@angular/cdk/bidi';
 import { Component, OnInit } from '@angular/core';
 import { GanttItem } from '@worktile/gantt';
 
@@ -8,11 +9,12 @@ import { GanttItem } from '@worktile/gantt';
 })
 export class ScheduleComponent implements OnInit {
 
+  aaa = 1661964830;
   items: GanttItem[] = [
     { id: '000000', title: 'Task 0', start: 1627729997, end: 1628421197, expandable: true },
     { id: '000001', title: 'Task 1', start: 1617361997, end: 1625483597, links: ['000003', '000004', '000000'], expandable: true },
     { id: '000002', title: 'Task 2', start: 1610536397, end: 1610622797 },
-    { id: '000003', title: 'Task 3', start: 1628507597, end: 1633345997, expandable: true }
+    { id: '000003', title: 'Task 3', start: 1628507597, end: 1633345997, expandable: true,progress: 0.3 }
   ];
   constructor() { }
 
