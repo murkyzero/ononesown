d9441d41c3678c3ac5617d78ffa26a4414bbd6e3
Howard <Howard@aitc.com.tw>
2022-09-04 00:37:07 +0800

    tmp3

--- frontend/src/app/employee/employee.component.ts
+++ frontend/src/app/employee/employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import {map} from "rxjs/operators";
+import { map } from "rxjs/operators";
 
 
 @Component({
@@ -11,10 +11,10 @@ import {map} from "rxjs/operators";
 export class EmployeeComponent implements OnInit {
 
   constructor(private http: HttpClient) {
-    this.http.get("api/v1/Employee")
+    this.http.get("api/v1/employee")
       .pipe(
-        map((res: any) => JSON.parse(res))
-      )
+      // map((res: any) => JSON.parse(res))
+    )
       .subscribe((res: any) => {
         console.log(res);
         console.log("BBB");
@@ -23,5 +23,18 @@ export class EmployeeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  /**
+   * 新增代辦事項
+   *
+   * @param {HTMLInputElement} inputRef - 輸入框的元素實體
+   * @memberof TodoListComponent
+   */
+  addEmployee( event: Event  ): void {
+    const value = (event.target as HTMLInputElement).value;
+    
 
+      // this.todoListService.add(todo);
+      console.log(value);
+      (event.target as HTMLInputElement).value='';
+  }
 }
